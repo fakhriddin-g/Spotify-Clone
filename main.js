5dcb78c568bce576022b8b212a6affeac26f5145
Fakhriddin <fakhmirakov1998@gmail.com>
2023-09-02 14:43:00 +0500

    start work with api

---
+++ main.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { userHTTP } from "./modules/http.request";
+
+const { request } = userHTTP()
+
+request("/v1/me/", "get").then(res => {
+  console.log(res);
+})
+
+
+// axios.get("https://api.spotify.com/v1/me/", {
+//   headers: {
+//     Authorization: "Bearer BQCORus3mYUHTV76MotkQ9XcE6TtJl91d9GrakxPz0hSAnujo284CVUjdURMS_U18jnZMSHTmGErd1dOXu1xORsApEZX3UxMCeFl8lSsNSpR99Rqcj7v0aL8tvIWBZa9ZiLVNG-YoA0jPK36n4Mu66J1Vsz_3n5uQxury0TV5qzEMBzp1n1lMt4EvO5y6iky4OmA1TmpOLXBCqCAt6VzTX8NirKrhz3mrAdkq_XU4gXGFDd-H8zr_tElyGAujQjmROrn1RP7DLmV-AfiEj9wy--j"
+//   }
+// }).then(res => {
+//   console.log(res);
+// })
---
+++ modules/http.request.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const baseURL = import.meta.env.VITE_API_KEY
+const token = import.meta.env.VITE_TOKEN
+
+const enums = {
+    get: "get",
+    post: "post",
+    patch: "patch",
+    put: "put",
+    delete: "delete"
+}
+
+export const userHTTP = () => {
+    const request = async (url, method) => {
+        if (!enums[method]) {
+            throw new Error("Axios has not provide method " + method)
+        }
+
+        try {
+            const res = await axios[method](baseURL + url, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+
+            if (res.status === 200 || res.status === 201) {
+                return res
+            }
+        } catch (error) {
+            return error
+        }
+    }
+
+    return {
+        request
+    }
+}
